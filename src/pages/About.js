02f9efd58e17628ef63fe6f306e7e0ff975a385d
Golu0512 <asif.hell111@gmail.com>
2023-09-07 18:43:57 +0530

    fixed about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,32 +3,34 @@ import React from 'react'
 const About = () => {
     return (
         <>
-            <div>
-                <p>
-                    Welcome to the Ultimate Anime Haven: Your Go-To Source for Seamless Anime Downloading and Streaming!
+            <div className='bg-dark text-white p-lg-5 p-md-5 p-2'>
+                
+                    <h4 className='text-center mb-3 fw-bold'>Welcome to the Ultimate Anime Haven: Your Go-To Source for Seamless Anime Downloading and Streaming!</h4>
 
-                    Indulge in your passion for anime like never before with our website dedicated to providing you with free and hassle-free access to a vast collection of anime movies and series. We're here to make your anime journey as enjoyable and convenient as possible, offering a seamless experience for both downloading and streaming, all without any pesky disturbances or redirects. And the best part? It's completely ad-free!
+                    <p className='text-center mx-lg-5 mx-md-5 mx-2 px-lg-5 px-md-5 px-2'>Indulge in your passion for anime like never before with our website dedicated to providing you with free and hassle-free access to a vast collection of anime movies and series. We're here to make your anime journey as enjoyable and convenient as possible, offering a seamless experience for both downloading and streaming, all without any pesky disturbances or redirects. And the best part? It's completely ad-free!</p>
 
-                    **Why Choose Our Anime Portal?**
+                    <p className='text-center m-0 py-4 fs-4 fw-bold text-uppercase text-warning'>**Why Choose Our Anime Portal?**</p>
 
-                    1. **Instant Access:** Say goodbye to cumbersome processes and time-consuming redirects. With our streamlined interface, you're just a few clicks away from your favorite anime content. Whether you're looking to download or stream, your adventure begins instantly.
+                    <div className='px-3'>
+                        <p><span className='fw-bold text-uppercase text-info'>1. **Request Your Favorites:**</span> Have a specific anime movie or series in mind? We're here to cater to your requests! Submit the title you're looking for, and our dedicated team will make it available within 24 hours. It's all about making your anime dreams come true.</p>
 
-                    2. **Ad-Free Environment:** We value your immersion and take pride in delivering an ad-free environment. Your anime experience should be uninterrupted, allowing you to fully immerse yourself in the captivating worlds and stories that anime offers.
+                        <p><span className='fw-bold text-uppercase text-info'>2. **Instant Access:**</span> Say goodbye to cumbersome processes and time-consuming redirects. With our streamlined interface, you're just a few clicks away from your favorite anime content. Whether you're looking to download or stream, your adventure begins instantly.</p>
 
-                    3. **Extensive Collection:** From classic masterpieces to the latest releases, our collection caters to all tastes and preferences. Action, romance, fantasy, thriller – whatever your genre, you'll find a diverse range of options awaiting your exploration.
+                        <p><span className='fw-bold text-uppercase text-info'>3. **Ad-Free Environment:**</span> We value your immersion and take pride in delivering an ad-free environment. Your anime experience should be uninterrupted, allowing you to fully immerse yourself in the captivating worlds and stories that anime offers.</p>
 
-                    4. **Seamless Streaming:** Prefer streaming over downloading? Our platform offers high-quality streaming that brings the vivid colors and engaging narratives of anime to life on your screen. It's like having your own personal screening room!
+                        <p><span className='fw-bold text-uppercase text-info'>4. **Extensive Collection:**</span> From classic masterpieces to the latest releases, our collection caters to all tastes and preferences. Action, romance, fantasy, thriller – whatever your genre, you'll find a diverse range of options awaiting your exploration.</p>
 
-                    5. **Request Your Favorites:** Have a specific anime movie or series in mind? We're here to cater to your requests! Submit the title you're looking for, and our dedicated team will make it available within 24 hours. It's all about making your anime dreams come true.
+                        {/* <p><span className='fw-bold text-uppercase text-info'>5. **Seamless Streaming:**</span> Prefer streaming over downloading? Our platform offers high-quality streaming that brings the vivid colors and engaging narratives of anime to life on your screen. It's like having your own personal screening room!</p> */}
 
-                    6. **Disturbance-Free Experience:** We understand the frustration of being bombarded with pop-ups and redirects while trying to enjoy your favorite anime. Our commitment to a disturbance-free environment ensures that your focus remains on what matters most – the anime itself.
+                        <p><span className='fw-bold text-uppercase text-info'>5. **Disturbance-Free Experience:**</span> We understand the frustration of being bombarded with pop-ups and redirects while trying to enjoy your favorite anime. Our commitment to a disturbance-free environment ensures that your focus remains on what matters most – the anime itself.</p>
 
-                    7. **Community Engagement:** Join our community of fellow anime enthusiasts. Share recommendations, engage in discussions, and discover hidden gems that might have slipped under your radar. Anime is meant to be enjoyed together, and our platform encourages just that.
+                        {/* <p><span className='fw-bold text-uppercase text-info'>7. **Community Engagement:**</span> Join our community of fellow anime enthusiasts. Share recommendations, engage in discussions, and discover hidden gems that might have slipped under your radar. Anime is meant to be enjoyed together, and our platform encourages just that.</p> */}
 
-                    Ready to embark on your anime odyssey? Look no further than our platform, where downloading and streaming anime is a breeze. With our user-centric approach and dedication to quality, your anime cravings will always find a satisfying solution.
+                        <p>Ready to embark on your anime odyssey? Look no further than our platform, where downloading and streaming anime is a breeze. With our user-centric approach and dedication to quality, your anime cravings will always find a satisfying solution.</p>
 
-                    Please note that our commitment to a smooth user experience doesn't compromise on copyright and licensing regulations. We respect the creators' rights and strive to provide a safe and legal platform for all anime lovers. Your journey into the mesmerizing world of anime begins now!
-                </p>
+                        <p>Please note that our commitment to a smooth user experience doesn't compromise on copyright and licensing regulations. We respect the creators' rights and strive to provide a safe and legal platform for all anime lovers. Your journey into the mesmerizing world of anime begins now!</p>
+                    </div>
+
             </div>
         </>
     )
